e5cadbfbeb41978a5429ed7abe0f459a6d81760d
quincydahal <126646749+quincydahal@users.noreply.github.com>
2024-01-08 20:02:21 +0545

    week1 tasks push

---
+++ week1/practical.01.py
@@ -0,0 +1,37 @@
+#1
+print("Hello World")
+
+#2
+print("Quincy Dahal")
+
+#3
+print("In celcius =",38.4,"In farenheit =", ((9/5)*38.4)+32)
+
+#4
+matches_played = 609
+times_batted = 1014
+times_not_out = 162
+runs_scored = 48426
+
+innings = times_batted - times_not_out
+average = runs_scored / innings
+
+print(f"Boycott's batting average is {average} runs")
+
+#5
+group1=113
+group2=175
+group3=12
+size=24
+
+l1=group1//size
+l1left=group1%size
+print(f"groups {l1} and left over students is {l1left}")
+
+l2=group2//size
+l2left=group2%size
+print(f"groups {l2} and left over students is {l2left}")
+
+l3=group3//size
+l3left=group3%size
+print(f"groups {l3} and left over students is {l3left}")
